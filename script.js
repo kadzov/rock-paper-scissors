4e9f80b0c2db870ae5b7bb338f2481049bdead67
przrk <76454345+przrk@users.noreply.github.com>
2021-04-14 02:01:36 +0300

    add

--- script.js
+++ script.js
@@ -1,37 +1,35 @@
-function game() {
-  let player = 0;
-  let computer = 0;
-  for (let i = 0; i < 5; i++) {
-    function computerPlay() {
-      let number = Math.floor(Math.random() * 3) + 1;
-      return number === 1 ? 'Rock'
-        : number === 2 ? 'Paper'
-          : 'Scissors';
-    }
-    function playRound(playerSelection, computerSelection) {
-      if (playerSelection === 'Rock' && computerSelection === 'Paper'
-        || playerSelection === 'Scissors' && computerSelection === 'Rock'
-        || playerSelection === 'Paper' && computerSelection === 'Scissors') {
-        computer++;
-        console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
-      } else if (playerSelection === 'Paper' && computerSelection === 'Rock'
-        || playerSelection === 'Rock' && computerSelection === 'Scissors'
-        || playerSelection === 'Scissors' && computerSelection === 'Paper') {
-        player++;
-        console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
-      } else {
-        console.log("Draw!");
-      }
-      console.log(`Player: ${player}, Computer: ${computer}`);
-    }
-    let playerSelection = prompt("What do you use?");
-    playerSelection = playerSelection.charAt(0).toUpperCase()
-      + playerSelection.slice(1).toLowerCase();
-    const computerSelection = computerPlay();
-    playRound(playerSelection, computerSelection);
-  }
-  return player > computer ? 'You Win!'
-    : player < computer ? 'You Lose!'
-      : 'Draw!';
+function computerPlay() {
+  let number = Math.floor(Math.random() * 3) + 1;
+  return number === 1 ? 'Rock'
+    : number === 2 ? 'Paper'
+      : 'Scissors';
 }
-console.log(game());
+let player = 0;
+let computer = 0;
+let i = 0;
+function playRound(playerSelection, computerSelection) {
+  if (i < 5) {
+    ++i;
+    if (playerSelection === 'Rock' && computerSelection === 'Paper'
+      || playerSelection === 'Scissors' && computerSelection === 'Rock'
+      || playerSelection === 'Paper' && computerSelection === 'Scissors') {
+      res.textContent = `Round ${i}\nYou Lose! ${computerSelection} beats ${playerSelection}\nPlayer: ${player}, Computer: ${++computer}`;
+    } else if (playerSelection === 'Paper' && computerSelection === 'Rock'
+      || playerSelection === 'Rock' && computerSelection === 'Scissors'
+      || playerSelection === 'Scissors' && computerSelection === 'Paper') {
+      res.textContent = `Round ${i}\nYou Win! ${playerSelection} beats ${computerSelection}\nPlayer: ${++player}, Computer: ${computer}`;
+    } else {
+      res.textContent = `Round ${i}\nDraw!\nPlayer: ${++player}, Computer: ${++computer}`;
+    }
+  } else {
+    res.textContent = player > computer ? 'You Win!'
+      : player < computer ? 'You Lose!'
+        : 'Draw!';
+  }
+}
+
+const res = document.querySelector('div');
+res.setAttribute('style', 'white-space: pre;');
+const rock = () => playRound('Rock', computerPlay());
+const paper = () => playRound('Paper', computerPlay());
+const scissors = () => playRound('Scissors', computerPlay());
